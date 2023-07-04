3ad9944547f1a1fcc9bfab6f2b3d2865fcc2edb8
jbirsa <jbirsa@itba.edu.ar>
2023-07-04 16:40:04 -0300

    added parsing functions

---
+++ fileParsing.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include "fileParsing.h"
+#include <string.h>
+#define MAX_CHAR 250
+#define NYC 0
+#define MON 1
+
+void parseStations(stationsADT stationsAdt, FILE *file, size_t identifier)
+{
+    const char delim[2] = ";";
+    char str[MAX_CHAR], *name;
+    size_t id;
+    fgets(str, MAX_CHAR, file); // obtiene la primera linea, que solamente aclara el formato.
+
+    while(fgets(str, MAX_CHAR, file) != NULL)
+    {
+        if(identifier == NYC) {
+            name = strtok(str,delim);
+            strtok(NULL,delim);
+            strtok(NULL,delim);
+            id = atoi(strtok(NULL,delim));
+        } else {
+            id = atoi(strtok(str,delim));
+            name = strtok(NULL,delim);
+        }
+        printf("%d %s\n",id,name);
+        //parseStation(stationsAdt, id, name);
+    }
+}
+
+void parseEvents(stationsADT stationsAdt, FILE *file, size_t identifier)
+{
+    const char delim[2] = ";";
+    char str[MAX_CHAR], isMember, *aux;
+    size_t fromId, toId, month;
+    fgets(str, MAX_CHAR, file); // obtiene la primera linea, que solamente aclara el formato.
+
+    while(fgets(str, MAX_CHAR, file) != NULL)
+    {
+        isMember = 0;
+        aux = strtok(str,delim);
+        fromId = atoi(strtok(NULL,delim));
+        strtok(NULL,delim);
+        toId = atoi(strtok(NULL,delim));
+
+        if(identifier == NYC) {
+
+            strtok(NULL,delim);
+            if(strcmp( strtok(NULL,delim), "member\n") == 0) //la linea del archivo termina con un '\n'
+                isMember = 1;
+        }
+        else {
+            isMember = atoi(strtok(NULL,delim));
+        }
+        strtok(aux,"-");
+        month = atoi(strtok(NULL,"-"));
+        printf("%d\t%d\t%d\t%d\n",month, fromId, toId, isMember);
+        //parseEvent(stationsAdt, month, fromId, toId, isMember);
+    }
+}
+
+int main()
+{
+    FILE *file = fopen("bikesNYC.csv","r");
+    stationsADT stations = newStations();
+    parseEvents(stations, file, NYC);
+    freeStations(stations);
+    return 0;
+}
---
+++ fileParsing.h
@@ -0,0 +1,19 @@
+/*
+    Archivo: fileParsing.h
+    ----------------------
+    Este archivo se encarga del parseo de los datos,
+    ya sea de las estaciones o de los eventos.
+*/
+
+#include <stdio.h>
+#include "stationsADT.h"
+
+#ifndef _fileparsing_h
+#define _fileparsing_h
+
+void parseStations(stationsADT stationsAdt, FILE *file, size_t identifier);
+
+void parseEvents(stationsADT stationsAdt, FILE *file, size_t identifier);
+
+
+#endif
