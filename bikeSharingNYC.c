1fdc1ff76e12d371e0a825a3ba43c6f743625da5
Josefina Gonzalez Cornet <117315719+jgonzalezcornet@users.noreply.github.com>
2023-07-05 20:17:13 -0300

    file opening procedures + resolved queries 1 and 3 (csv)

--- bikeSharingNYC.c
+++ bikeSharingNYC.c
@@ -1,26 +1,92 @@
 #include <stdio.h>
 #include "stationsADT.h"
+
 #define NYC 0
-
-
 #define DELIM ";"
+#define MONTHS 12
+#define QUERIES 3
+
+// Función para cerrar todos los archivos
+void closeFiles (size_t count, FILE * files[]);
 
 int main(int argc, char *argv[]) {
-
-    if(argc < 3) {
-        printf("Cantidad insuficiente de parametros\n");
-        printf("Uso: ./bikeSharingMON archivo_data_alquileres archivo_data_estaciones\n");
-        return 1;
+    if(argc != 3) {
+        fprintf(stderr, "Cantidad inválida de parámetros.\n");
+        fprintf(stdin, "Uso: ./bikeSharingNYC archivo_data_alquileres archivo_data_estaciones\n");
+        exit(1);
     }
 
+    // Abrimos todos los archivos, estableciendo el errno en 0 para poder chequear si hay errores en la apertura
+    errno = 0;
+    FILE * events = fopen(argv[1], "r");
+    FILE * stations = fopen(argv[2], "r");
+    FILE * query1 = fopen(query1.csv, "wt");
+    FILE * query2 = fopen(query2.csv, "wt");
+    FILE * query3 = fopen(query3.csv, "wt");
+    FILE * files[] = {events, stations, query1, query2, query3};
+    size_t fileCount = QUERIES + argc - 1;
+    
+    if (errno==ENOENT){
+        closeFiles(fileCount, files); // si alguno de los archivos no se pudo abrir, cierro todos
+        fprintf(stderr, "No se pudo abrir alguno de los archivos.\n");
+        exit(1);
+    }
+    
     stationsADT stationsAdt = newStations();
-
+    if (stationsAdt == NULL){
+        fprintf(stderr, "No hay memoria suficiente para llevar a cabo el programa.\n");
+        closeFiles(fileCount, files);
+        exit(1);
+    }
+    
     // Carga de datos
+    parseEvents(stationsAdt, events, NYC);
+    parseStations(stationsAdt, stations, NYC);
 
-    //processEvent(stationsAdt, argv[1]);
-    addStations(stationsAdt, argv[2], NYC);
+   /* -------------- Resolución de las queries en HTML --------------*/
+    query1(stationsAdt);
+    query2(stationsAdt);
+    query3(stationsAdt);
 
+    
+    /* -------------- Resolución de la query 1 (csv) --------------*/
+    fprintf(query1, "Station;StartedTrips\n");
+    toBeginTrip(stationsAdt);
+    do {
+        fprintf(query1, "%s;%zu\n", stationsAdt->itTrip->name, stationsAdt->itTrip->quanTripsTotal);
+    } while (nextTrip(stationsAdt));
+    fclose(query1);
+
+    /* -------------- Resolución de la query 2 (csv) --------------*/
+
+    
+    /* -------------- Resolución de la query 3 (csv) --------------*/
+    fprintf(query3, "J;F;M;A;M;J;J;A;S;O;N;D;Station");
+    toBeginName(stationsAdt);
+    size_t count;
+    do {
+        for (size_t i=0 ; i<MONTHS ; i++){
+            count = stationsAdt->itName->quanTrips[i];
+            fprintf(query3, "%zu;", count);
+        }
+        fprintf(query3, "%s\n", stationsAdt->itName->name);
+    } while (nextName(stationsAdt));
+    fclose(query3);
+
+    // Liberacion de memoria
     freeStations(stationsAdt);
-
+    // Cerramos los files que hayan quedado abiertos
+    closeFiles(fileCount, files);
+    
     return 0;
+}
+
+void closeFiles (size_t count, FILE * files[]){
+    for (size_t i=0 ; i < count ; i++){
+        if (files[i]!=NULL){
+            fclose(files[i]);
+        }
+    }
+    return;
+}
 }
